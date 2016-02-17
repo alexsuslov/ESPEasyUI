f9a38f0701e7cf038c171bcdd99ec7d6d64c1a1f
4refr0nt <4refr0nt@gmail.com>
2016-02-17 22:43:45 +0500

    upd

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
-# EspeasyUI
-## Css
+# ESPEasyUI
+## css
 https://www.bootstrapcdn.com/bootswatch/
+
+## preReq
+node.js
+
+## Build
+go to Arduino libraries folder then do:
+```
+git clone https://github.com/alexsuslov/ESPEasyUI.git
+cd ESPEasyUI
+npm i
+
+```
+for MAC and Linux
+```
+./build.sh
+```
+for Windows
+```
+build-win.cmd 
+```
---
+++ build-win.cmd
@@ -0,0 +1,19 @@
+@Echo off
+cls
+echo -----------------------------------------
+echo      Start generating UI for ESPEasy
+echo -----------------------------------------
+del *.h > nul
+call gulp dist
+
+cd html
+
+for %%f in (*.*) do (
+  echo Processing: %%f
+  C:\cygwin64\bin\xxd -i %%f > %%~nf.h
+  C:\cygwin64\bin\sed "s/unsigned char %%~nf\_html\[\] \=/const char %%~nf_html[] PROGMEM =/" %%~nf.h | sed "s/unsigned int/const int/" >../%%~nf.h
+  echo Resulting : %%~nf.h
+)
+echo -----------------------------------------
+echo                 Done. 
+echo -----------------------------------------
